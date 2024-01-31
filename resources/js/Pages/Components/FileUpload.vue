faf00252047dc215ba3437f42b1a849a2fb49c3c
LuisGuervos <72911548+LuisGuervos@users.noreply.github.com>
2024-01-31 20:07:25 +0100

    Update FileUpload.vue
    Invoke URL

--- resources/js/Pages/Components/FileUpload.vue
+++ resources/js/Pages/Components/FileUpload.vue
@@ -78,7 +78,7 @@ const onFileChange = (event) => {
 
     const formData = new FormData()
     formData.append('file', file)
-    const url = ''
+    const url = 'https://b9qow81943.execute-api.us-east-1.amazonaws.com/dev'
     const config = {
         headers: {
             'Content-Type': 'text/csv'
